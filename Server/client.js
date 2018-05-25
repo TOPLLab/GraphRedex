102b10d136b4bbfd909fff6829351ad94cb78a5f
Christophe Scholliers <christophe.scholliers@ugent.be>
2018-05-25 15:06:29 +0200

    Parsing json on the web

--- Server/client.js
+++ Server/client.js
@@ -1,16 +1,19 @@
 window.onload = function() {
-    // var sock = new WebSocket("ws://localhost:8081/test", "subprotocol");
     var sock = new WebSocket("ws://localhost:8081/");
     var messagecounter = 0;
     sock.onopen = function() {
         console.log("open", arguments);
     };
     sock.onmessage = function(e) {
-        console.log("message", e.data);
-        messagecounter++;
-        if (messagecounter == 10) {
-            sock.send("goodbye");
+        var obj = JSON.parse(e.data);
+        for (var i in obj) {
+            console.log(obj[i]);
+            for (k in obj[i]) {
+                console.log("K: " + k + " V: " + obj[i][k]);
+            }
         }
+
+        //console.log('message', e.data);
     };
     sock.onclose = function() {
         console.log("close", arguments);
