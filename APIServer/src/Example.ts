dd085e4d8cbc282fc4ace5ee17f60053b2890cbd
beardhatcode <mail.garonn@gmail.com>
2018-11-27 18:11:03 +0100

    feat(Example): get next nodes via API

--- APIServer/src/Example.ts
+++ APIServer/src/Example.ts
@@ -32,11 +32,43 @@ export default class Example {
     }
 
     /**
+     * Gets the outbound nodes within `steps` steps from any of the nodes in
+     * bases (specified by _key)
+     * @param bases the _key's of the nodes to start form (graph will be prepended)
+     * @param steps number of reductions
+     */
+    public async extend(bases: string[], steps: number = 1) {
+        if (steps < 0) {
+            throw "steps must be positive";
+        }
+        const qry = aql`
+        LET nodes = (
+            FOR docId IN ${bases.map((x) => this.name + "/" + x)}
+                FOR v IN 0..${steps}
+                    OUTBOUND docId GRAPH ${this.name}
+                    OPTIONS {bfs:true,uniqueVertices: 'global'}
+                    RETURN DISTINCT v)
+        LET edges = (
+            FOR a in nodes
+                FOR e IN ${this.edgeCollection}
+                    FILTER  e._from == a._id OR e._to == a._id
+                        RETURN DISTINCT e)
+        RETURN {nodes,edges}`;
+
+        return await this.db.ro
+            .query(qry)
+            .then((cursor) => cursor.all())
+            .then((keys) =>
+                Object.assign({ meta: { baseTerms: bases } }, keys[0]),
+            );
+    }
+
+    /**
      *
      * @param from  The start term, must be in the example, should be of the from "example-1/5445454" (the examples base term)
      * @param steps
      */
-    public async showAll(from: String = null, steps: number = 200) {
+    public async showAll(from: String = null, steps: number = 300) {
         if (from === null) {
             from = this.baseTerm;
         }
