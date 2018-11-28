1b85d25e8fc7481aa7362be4f8971bf3842e644e
beardhatcode <mail.garonn@gmail.com>
2018-11-28 19:23:57 +0100

    feat(initial setup): Add initialistion script

--- APIServer/index.js
+++ APIServer/index.js
@@ -16,18 +16,6 @@ console.log(process.argv);
 
 require("./dist/Server.js")
     .default.bootstrap(datadir)
-    .then((server) => {
-        server.app.set("port", process.env.PORT || 3000);
-
-        server.app.use("/", express.static(public));
-        server.app.get("/", (req, res) => {
-            res.sendFile(path.join(public, "index.html"));
-        });
-        const s = server.app.listen(server.app.get("port"), function() {
-            console.log("Express server listening on port " + s.address().port);
-            console.log("Datadir is " + datadir);
-        });
-    })
     .catch((e) => {
         console.log(e.code);
         switch (e.code) {
@@ -39,4 +27,30 @@ require("./dist/Server.js")
             default:
                 console.log(e.code);
         }
-    });
+    })
+    .then((server) => {
+        server.app.set("port", process.env.PORT || 3000);
+        server.app.use("/", express.static(public));
+        server.app.get("/", (req, res) => {
+            res.sendFile(path.join(public, "index.html"));
+        });
+        const s = server.app
+            .listen(server.app.get("port"), function() {
+                console.log(
+                    "Express server listening on port " + s.address().port,
+                );
+                console.log("Datadir is " + datadir);
+            })
+            .on("error", function(err) {
+                if (err.errno === "EADDRINUSE") {
+                    console.log(
+                        "Could not setup server, is port " +
+                            server.app.get("port") +
+                            " already in use?",
+                    );
+                } else {
+                    throw err;
+                }
+            });
+    })
+    .catch((x) => console.error(x));
