b46d735244e0f28dda754b39e727d53989ad8714
beardhatcode <mail.garonn@gmail.com>
2018-11-28 19:23:57 +0100

    feat(initial setup): Add initialistion script

---
+++ APIServer/src/SetupAndClear.ts
@@ -0,0 +1,124 @@
+import MyDatabase from "./Database";
+
+function logLn(text: string) {
+    const minWidth = 50;
+    if (text.length < minWidth - 8) {
+        text += " ".repeat(minWidth - 8 - text.length);
+    }
+    console.log("┏" + "━".repeat(text.length + 2) + "┓");
+    console.log(`┃ ${text} ┃`);
+    console.log("┗" + "━".repeat(text.length + 2) + "┛");
+}
+
+async function prepare() {
+    const db = await MyDatabase.bootstrap();
+
+    logLn(`Removing all documents form DB...`);
+    await db.rw.truncate();
+    console.log("done");
+
+    logLn(`Renewing collections and graph...`);
+
+    console.log("Deleting all collections");
+    await Promise.all(
+        await db.rw.collections().then((cs) =>
+            cs.map(async (c) => {
+                // tslint:disable-next-line
+                await c["drop"]();
+                console.log(` - ${c.name} dropped`);
+            }),
+        ),
+    );
+
+    console.log("Deleting all Graphs");
+    await Promise.all(
+        await db.rw.graphs().then((cs) =>
+            cs.map(async (c) => {
+                // tslint:disable-next-line
+                await c["drop"]();
+                console.log(` - ${c.name} dropped`);
+            }),
+        ),
+    );
+
+    console.log("Creating collections");
+    const languagesCollection = db.rw.collection("languages");
+    const usersLanguagesCollection = db.rw.edgeCollection("users-languages");
+    const usersCollection = db.rw.collection("users");
+    const examplesCollection = db.rw.collection("examples");
+    const usersExamplesCollection = db.rw.edgeCollection("users-examples");
+    const examplesGraph = db.rw.graph("Data");
+
+    await Promise.all(
+        [
+            languagesCollection,
+            usersLanguagesCollection,
+            usersCollection,
+            examplesCollection,
+            usersExamplesCollection,
+        ].map(async (c) => {
+            await c.create().catch((e) => {
+                console.log(` ! ${c.name} could not be created`);
+                throw e;
+            });
+            console.log(` - ${c.name} collection created`);
+        }),
+    );
+
+    if (!(await usersCollection.documentExists("1"))) {
+        logLn("Create demo:demo");
+        await usersCollection.save({
+            _key: "1",
+            name: "demo",
+            password: {
+                salt: "B0+PC18=",
+                hash:
+                    // tslint:disable-next-line:max-line-length
+                    "F9LRnLwgcaqW1IBDEhtNgSX6gLbX6Ye1AtTAnMmal0CPERVs1odDLxX85F02dpg05/CyS6RiPsirMF8FnH+PX3uU7NIznx7eAzNoWFV57zuEgqbEevnJbp5eDzbji5TceCMesZ/Mz0AeGc9LV/jz4OiEhEKLRGEpmKCi0J1YJSJTSLBMd62P1UsCV1Mv5UOZYrwNAqnzvpfqN17Qz4x8ZWQsaJdrEpMiG/Bav4z7LL+xtmbPeEEOh2W0Gk01cuSc+U2la0rwV81uh16lKXp1nUyOo9qY3G+IbR1CnFFrcnud3kSipqjtzrH/UiDBFzyTV61MbZvx8c/BubrIL7LaafLA0VUsNo0JNDpmnu6EQtO4+0mqBPUG5njLo+hsaMPSKfIvXTQZq5nAsC1QmXH69UB+tW82JPxyEa4U5fKADJKl5QhnjoBzgXzvhhVSGIsu3hRIi0a4ZG7GxGUUKEqU6TcONFda1+W+tRpnFqylD3ogT7ikvjWDJeG8AaDeRmx+dCn3Fmp49yqDAAp25D+DuVClcCL4Uns69ge4tKDaQ9W8yhtHjjaPXFc45KU+sYcp7DkdLvKTUcIoF/aClELYOp2anOhMtDbONUS/xuSZCqRYcIRDSSUFobes2cm0ggXtTlpmc2yjO6/RjqaiebkAA+YloabmSr//nGaIuDmXd5U=",
+                iterations: 10000,
+            },
+        });
+        console.log("Demo user added (_key = 1, demo:demo)");
+    }
+
+    logLn("renew examples graph");
+    if (await examplesGraph.exists()) {
+        console.log("Deleting");
+        await examplesGraph.drop();
+    }
+
+    console.log("Creating");
+    await examplesGraph.create({
+        edgeDefinitions: [
+            {
+                collection: usersExamplesCollection.name,
+                from: [usersCollection.name],
+                to: [examplesCollection.name],
+            },
+            {
+                collection: usersLanguagesCollection.name,
+                from: [usersCollection.name],
+                to: [languagesCollection.name],
+            },
+        ],
+    });
+
+    console.log("Examples per user graph created");
+
+    return;
+}
+
+prepare()
+    .then(() => console.log("done"))
+    .catch((e) => {
+        if (e.response) {
+            console.log(
+                "error",
+                e.response.body,
+                e.response.request.path,
+                e.response.request.method,
+            );
+        } else {
+            console.log(e);
+        }
+    });
--- APIServer/start.sh
+++ APIServer/start.sh
@@ -3,17 +3,48 @@
 # By Robbert Gurdeep Singh
 ################################################################################
 
-if [[ $# -eq 1 ]]; then
-set -e
-yarn run grunt
+run=1
+dirSet=0
+
+while getopts "h?bcnd:" opt; do
+    case "$opt" in
+        h|\?)
+            echo "-b to build"
+            echo "-d to set directory"
+            echo "-c to clean db"
+            echo "-n to not run"
+            exit 0
+            ;;
+        b)  yarn run grunt
+            ;;
+        c)  node dist/Bootstrapper.js
+            echo "ensure that the datadir is emptied"
+            ;;
+        n)  run=0
+            ;;
+        d)  dirSet=1
+            MYTMPDIR=$OPTARG
+            ;;
+    esac
+done
+
+if [[ $run -eq 1 ]]
+then
+    if [[ $dirSet -eq 0 ]]
+    then
+        echo "Making tmp dir"
+        MYTMPDIR=$(mktemp -d)
+        trap "echo STOP;rm -rf $MYTMPDIR" EXIT
+    fi
+
+    MYTMPDIR=${MYTMPDIR%/}
+    if [[ -d "$MYTMPDIR" ]]
+    then
+        ./index.js "$MYTMPDIR"
+    else
+        echo "$MYTMPDIR is not a directory"
+    fi
+
+else
+    echo "Not running"
 fi
-
-MYTMPDIR=$(mktemp -d)
-trap "echo STOP;rm -rf $MYTMPDIR" EXIT
-
-mkdir -p "$MYTMPDIR/1/lang/1"
-cp  $(dirname $0)"/../RedexServer/Jugs.rkt" "$MYTMPDIR/1/lang/1/main.rkt"
-mkdir -p "$MYTMPDIR/1/lang/2"
-cp  $(dirname $0)"/../RedexServer/Test.rkt" "$MYTMPDIR/1/lang/2/main.rkt"
-
-./index.js "$MYTMPDIR"
