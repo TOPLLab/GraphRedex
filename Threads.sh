d7942bc402452cdc5dd6111713a0e1ffcf9d8016
beardhatcode <mail.garonn@gmail.com>
2018-12-03 17:17:26 +0100

    clean(*): Remove old files and restructure

--- Threads.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Name:
-# By Robbert Gurdeep Singh
-################################################################################
-echo "((store (x 1)) (threads (set! x (+ x -1)) (set! x (+ x 1))))" | LC_ALL=C racket RedexServer/Threads.rkt
