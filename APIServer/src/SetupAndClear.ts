2bd207d5916e1ac3ff817a1dbc52392ad4318680
beardhatcode <mail.garonn@gmail.com>
2019-03-04 18:26:24 +0100

    feat(setup): Add option to prefill password from file for demo

--- APIServer/src/SetupAndClear.ts
+++ APIServer/src/SetupAndClear.ts
@@ -1,4 +1,7 @@
 import MyDatabase from "./Database";
+import * as os from "os";
+import * as path from "path";
+import { isReadableFile } from "./Utils";
 
 function logLn(text: string) {
     const minWidth = 50;
@@ -66,19 +69,20 @@ export async function prepare() {
     );
 
     if (!(await usersCollection.documentExists("1"))) {
-        logLn("Create demo:demo");
+        logLn("Create demo user");
+        const [isDemo, passwd] = await getPass();
+        console.log(passwd);
         await usersCollection.save({
             _key: "1",
             name: "demo",
-            password: {
-                salt: "B0+PC18=",
-                hash:
-                    // tslint:disable-next-line:max-line-length
-                    "F9LRnLwgcaqW1IBDEhtNgSX6gLbX6Ye1AtTAnMmal0CPERVs1odDLxX85F02dpg05/CyS6RiPsirMF8FnH+PX3uU7NIznx7eAzNoWFV57zuEgqbEevnJbp5eDzbji5TceCMesZ/Mz0AeGc9LV/jz4OiEhEKLRGEpmKCi0J1YJSJTSLBMd62P1UsCV1Mv5UOZYrwNAqnzvpfqN17Qz4x8ZWQsaJdrEpMiG/Bav4z7LL+xtmbPeEEOh2W0Gk01cuSc+U2la0rwV81uh16lKXp1nUyOo9qY3G+IbR1CnFFrcnud3kSipqjtzrH/UiDBFzyTV61MbZvx8c/BubrIL7LaafLA0VUsNo0JNDpmnu6EQtO4+0mqBPUG5njLo+hsaMPSKfIvXTQZq5nAsC1QmXH69UB+tW82JPxyEa4U5fKADJKl5QhnjoBzgXzvhhVSGIsu3hRIi0a4ZG7GxGUUKEqU6TcONFda1+W+tRpnFqylD3ogT7ikvjWDJeG8AaDeRmx+dCn3Fmp49yqDAAp25D+DuVClcCL4Uns69ge4tKDaQ9W8yhtHjjaPXFc45KU+sYcp7DkdLvKTUcIoF/aClELYOp2anOhMtDbONUS/xuSZCqRYcIRDSSUFobes2cm0ggXtTlpmc2yjO6/RjqaiebkAA+YloabmSr//nGaIuDmXd5U=",
-                iterations: 10000,
-            },
+            password: passwd,
         });
-        console.log("Demo user added (_key = 1, demo:demo)");
+        console.log("Demo user added (_key = 1, demo:????)");
+
+        if (isDemo) {
+            console.warn("WARNING: password for demo is demo!");
+            console.warn("DO NOT USE IN PRODUCTION");
+        }
     }
 
     logLn("renew examples graph");
@@ -106,3 +110,24 @@ export async function prepare() {
     console.log("Examples per user graph created");
     return;
 }
+
+async function getPass(): Promise<[boolean, Object]> {
+    const passwdPath = path.join(os.homedir(), "graphredex-login.json");
+    if (await isReadableFile(passwdPath)) {
+        return [false, await require(passwdPath)];
+    } else {
+        return [
+            true,
+            {
+                // default pass: "demo"
+                salt: "B0+PC18=",
+                hash:
+                    // tslint:disable-next-line:max-line-length
+                    "F9LRnLwgcaqW1IBDEhtNgSX6gLbX6Ye1AtTAnMmal0CPERVs1odDLxX85F02dpg05/CyS6RiPsirMF8FnH+PX3uU7NIznx7eAzNoWFV57zuEgqbEevnJbp5eDzbji5TceCMesZ/Mz0AeGc9LV/jz4OiEhEKLRGEpmKCi0J1YJSJTSLBMd62P1UsCV1Mv5UOZYrwNAqnzvpfqN17Qz4x8ZWQsaJdrEpMiG/Bav4z7LL+xtmbPeEEOh2W0Gk01cuSc+U2la0rwV81uh16lKXp1nUyOo9qY3G+IbR1CnFFrcnud3kSipqjtzrH/UiDBFzyTV61MbZvx8c/BubrIL7LaafLA0VUsNo0JNDpmnu6EQtO4+0mqBPUG5njLo+hsaMPSKfIvXTQZq5nAsC1QmXH69UB+tW82JPxyEa4U5fKADJKl5QhnjoBzgXzvhhVSGIsu3hRIi0a4ZG7GxGUUKEqU6TcONFda1+W+tRpnFqylD3ogT7ikvjWDJeG8AaDeRmx+dCn3Fmp49yqDAAp25D+DuVClcCL4Uns69ge4tKDaQ9W8yhtHjjaPXFc45KU+sYcp7DkdLvKTUcIoF/aClELYOp2anOhMtDbONUS/xuSZCqRYcIRDSSUFobes2cm0ggXtTlpmc2yjO6/RjqaiebkAA+YloabmSr//nGaIuDmXd5U=",
+                iterations: 10000,
+            },
+        ];
+    }
+}
+
+prepare().then(console.log);
