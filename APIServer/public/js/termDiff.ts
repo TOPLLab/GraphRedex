ce310b3febb628c995dde0e135f1367527ec0a2a
beardhatcode <mail.garonn@gmail.com>
2019-05-02 18:31:32 +0200

    Addapt termDiff to work with formatted text

--- APIServer/public/js/termDiff.ts
+++ APIServer/public/js/termDiff.ts
@@ -7,30 +7,46 @@ export default function termDiff(
     t1: string,
     t2: string,
     markup: TermDiffMarkup = f,
-) {
+): string {
     const d1 = destructureTerm(t1);
     const d2 = destructureTerm(t2);
     structureDiff(d1, d2);
-    return [visualise(d1, markup), visualise(d2, markup)];
+    return visualise(d1, t1, markup).text;
 }
 
-function visualise(d1: Decoded, f: TermDiffMarkup) {
+function visualise(d1: Decoded, t1: string, f: TermDiffMarkup): TextPiece {
     if (isArray(d1.content)) {
-        return f(
-            d1.content.map((x) => visualise(x, f)).join(" "),
-            d1.isDifferent,
-        );
+        if (d1.content.length > 0) {
+            let result = visualise(d1.content[0], t1, f);
+            for (let i = 1; i < d1.content.length; i++) {
+                const cur = visualise(d1.content[i], t1, f);
+                while (result.to + 1 < cur.from) {
+                    result.to++;
+                    result.text += t1[result.to];
+                } // result.to + 1 == cur.from
+                result.text += cur.text;
+                result.to = cur.to;
+            }
+            result.text = f(result.text, d1.isDifferent);
+            return result;
+        } else {
+            throw "Unexpected end of diff";
+        }
     } else {
-        return f(d1.content, d1.isDifferent);
+        return {
+            text: f(d1.content.text, d1.isDifferent),
+            from: d1.content.from,
+            to: d1.content.to,
+        };
     }
 }
 
 type Decoded = {
     isDifferent: boolean;
-    content: Array<Decoded> | string;
+    content: Array<Decoded> | TextPiece;
 };
 
-function isArray(arg: any): arg is Array<Decoded | string> {
+function isArray(arg: any): arg is Array<Decoded | TextPiece> {
     return Array.isArray(arg);
 }
 
@@ -54,44 +70,60 @@ function structureDiff(d1: Decoded, d2: Decoded) {
             return true;
         } else {
             // strings
-            const different = d1.content !== d2.content;
+            const different = d1.content.text !== d2.content.text;
             d1.isDifferent = different;
             d2.isDifferent = different;
+            if (different) {
+                console.log(d1.content.text, d2.content.text);
+            }
             return different;
         }
     }
 }
 
-type RawDecoded = RawDecodedArray | string | null;
+type RawDecoded =
+    | RawDecodedArray
+    | { text: string; from: number; to: number }
+    | null;
+type TextPiece = { text: string; from: number; to: number };
 interface RawDecodedArray extends Array<RawDecoded> {}
+
+function isTextpiece(arg: RawDecoded): arg is TextPiece {
+    return arg !== null && "text" in arg;
+}
 
 export function destructureTerm(data: string): Decoded {
     let d: RawDecoded = [];
     let ptrs = [d];
 
     let curPtr = () => ptrs[ptrs.length - 1];
-    let addChar = (c: string) => {
+    let addChar = (c: string, pos: number, join = true) => {
         const cur = curPtr();
-        if (cur.length > 0 && typeof cur[cur.length - 1] === "string") {
-            cur[cur.length - 1] += c;
-        } else {
-            cur.push(c);
+        if (join && cur.length > 0) {
+            const curLast = cur[cur.length - 1];
+            if (isTextpiece(curLast)) {
+                curLast.text += c;
+                curLast.to = pos;
+                return;
+            }
         }
+        cur.push({ text: c, from: pos, to: pos });
     };
 
-    for (const c of data) {
+    for (let i = 0; i < data.length; i++) {
+        const c = data[i];
         switch (c) {
             case "(":
                 let newC = [];
                 curPtr().push(newC);
                 ptrs.push(newC);
-                curPtr().push(c);
+                addChar(c, i, false);
                 curPtr().push(null);
 
                 break;
             case ")":
                 curPtr().push(null);
-                curPtr().push(c);
+                addChar(c, i);
                 ptrs.pop();
                 break;
             case " ":
@@ -101,7 +133,7 @@ export function destructureTerm(data: string): Decoded {
 
                 break;
             default:
-                addChar(c);
+                addChar(c, i);
         }
     }
 
