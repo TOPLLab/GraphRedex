ab7425c3d1384a278d622413dec3c49001b02c88
beardhatcode <Robbert.GurdeepSingh@UGent.be>
2019-12-30 12:57:07 +0100

    pretier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    trailingComma: "all",
+    arrowParens: "always",
+    singleQuote: false,
+    jsxSingleQuote: false,
+    tabWidth: 4,
+    semi: true,
+    quoteProps: "consistent",
+    arrowParens: "always",
+    endOfLine: "lf",
+};
