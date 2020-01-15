f446621082a3ba91c11332b9e42bb5c8b30400b0
beardhatcode <Robbert.GurdeepSingh@UGent.be>
2020-01-15 12:46:39 +0100

    Simplify echo server
    Remove ASCII art
    Reduce number of params to process
    Remove unused funcions
    General cleanup

--- RedexServer/GenericServerCode/EchoServer.rkt
+++ RedexServer/GenericServerCode/EchoServer.rkt
@@ -1,22 +1,9 @@
 #lang racket
 (provide run-echo)
 (require redex/reduction-semantics )
+(require redex/pict )
 (require file/convertible)
-(require json)
 (require "./ArangoDB.rkt")
-
-
-
-;  ________                    .__      __________           .___
-; /  _____/___________  ______ |  |__   \______   \ ____   __| _/____ ___  ___
-;/   \  __\_  __ \__  \ \____ \|  |  \   |       _// __ \ / __ |/ __ \\  \/  /
-;\    \_\  \  | \// __ \|  |_> >   Y  \  |    |   \  ___// /_/ \  ___/ >    <
-; \______  /__|  (____  /   __/|___|  /  |____|_  /\___  >____ |\___  >__/\_ \
-;        \/           \/|__|        \/          \/     \/     \/    \/      \/
-; Christophe.Scholliers@UGent and Thomas.Dupriez@ens-paris-saclay.fr
-;
-
-
 
 
 
@@ -24,32 +11,40 @@
 
   (define db (arango-new "graphredex-data" graphname))
 
-  (define (trans->json t ts trans)
-    (jsexpr->string
-     (make-hash
-      (list
-       (cons 'from (expr->string t))
-       (cons 'next (map trans ts))))))
-
   (define (expr->string e) (format "~s" e))
 
+  (define (lolz reductions)
+    (define (f name)
+      (render-reduction-relation-rules `(,name))
+      `(,(format "~s" name) . ,(render-reduction-relation reductions))
+      )
 
+    (map f (reduction-relation->rule-names reductions))
+    )
 
 
   (define (node-done? term)
-    (let ([res (hash-ref (arango-qry db
-                                     "FOR doc IN @@tcol FILTER doc.term == @term AND doc._expanded==true LIMIT 1 RETURN doc._id"
-                                     `#hash((term . ,(expr->string term) )   )
-                                     #t
-                                     )
-                         'result)])
+    (let ([res 
+           (hash-ref 
+            (arango-qry db
+                        "FOR doc IN @@tcol 
+                           FILTER doc.term == @term AND doc._expanded==true 
+                           LIMIT 1
+                           RETURN doc._id"
+                        `#hash((term . ,(expr->string term) )   )
+                        #t
+                        )
+            'result)])
       (> (length res) 0)
       )
     )
 
 
+  (define (pict->svg pict)
+    (bytes->string/utf-8 (convert pict 'svg-bytes))
+    )
 
-  (define (make-node-data term trans)
+  (define (make-node-data term)
     (let* (
            [user-data (trans term)]
            [full-data (make-hash
@@ -60,7 +55,7 @@
       ; Convert pict to svg is it exists
       (cond [(hash-has-key? full-data '_pict)
              (hash-set! full-data '_pict
-                        (bytes->string/utf-8 (convert (hash-ref full-data '_pict) 'svg-bytes)))
+                        (pict->svg (hash-ref full-data '_pict)))
              
              ])
       
@@ -70,18 +65,20 @@
     )
 
 
-  (define (process term-stream reductions-left relation trans)
+  (define (process term-stream reductions-left)
     (cond
       ; Stop if out of runs
-      [(= 0 reductions-left) (fprintf (current-error-port) "\nOUT OF REDUCTIONS\n")]
+      [(= 0 reductions-left) 
+       (fprintf (current-error-port) "\nOUT OF REDUCTIONS\n")]
 
       ; Stop if no more terms to proccess
-      [(stream-empty? term-stream) (fprintf (current-error-port) "\nDONE (~a reductions left)\n" reductions-left)]
+      [(stream-empty? term-stream)
+       (fprintf (current-error-port) "\nDONE (~a reductions left)\n" reductions-left)]
 
       ; Skip is tern us already processed
       [(node-done? (stream-first term-stream))
        ;(fprintf (current-error-port) "\nSKIPPED\n")
-       (process (stream-rest term-stream) reductions-left relation trans)
+       (process (stream-rest term-stream) reductions-left)
        ]
 
       ; Expand the term at the head of the stream
@@ -92,16 +89,20 @@
             (term (stream-first  term-stream))
             (next-terms (apply-reduction-relation/tag-with-names relation term))
             )
-         (arango-make-node db (make-node-data term trans) #t (zero? (length next-terms)))
+         (arango-make-node db (make-node-data term) #t (zero? (length next-terms)))
 
          (for ([x next-terms])
            (match x [`(,rel ,term2)
                      ;(fprintf (current-error-port) "\nAdded: ~a -[reduces:~a]-> ~a\n" term rel term2)
-                     (arango-make-node db (make-node-data term2 trans) #f #f)
+                     (arango-make-node db (make-node-data term2) #f #f)
                      (arango-make-edge db (expr->string term) (expr->string term2) rel)
                      ])
            )
-         (process (stream-append (stream-rest term-stream) (map (lambda (x) (car (cdr x))) next-terms)) (- reductions-left 1) relation trans)
+         (process 
+          (stream-append
+           (stream-rest term-stream)
+           (map (lambda (x) (car (cdr x))) next-terms)) 
+          (- reductions-left 1))
          )]
       )
 
@@ -111,13 +112,11 @@
 
 
 
-  (let*
-      (
-       (term (read-term))
-       )
-    (arango-make-node db (make-node-data term trans) #f #f )
+  ; "main"
+  (let ([term (read-term)])
+    (arango-make-node db (make-node-data term) #f #f )
     (display (car (arango-lookup db (expr->string term))))
-    (process (stream term) redLimit relation trans)
+    (process (stream term) redLimit)
     )
 
   )
