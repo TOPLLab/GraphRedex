d87b4bcfcaf82b275931f595bbcfcc7a9aac7a31
beardhatcode <Robbert.GurdeepSingh@UGent.be>
2020-01-19 18:21:08 +0100

    clean treeForce a bit
    dtill not realy happy with this

--- APIServer/public/js/shower/treeForce.ts
+++ APIServer/public/js/shower/treeForce.ts
@@ -4,31 +4,30 @@
  * D3 force that calculates the Depth of all the nodes according to the links
  * and pulls the nodes to x pos `intervalLen` * depth.
  *
- * @param intervalLen      Distance between nodes of subsequent depths
- * @param startNodeGetter  function that returns the current start node id
- * @todo  Maybe we need a depth getter here...
+ * @param dist      Distance between nodes of subsequent depths
+ * @param getStart  function that returns the current start node id
+ * @todo  Refactor into class once classes can be callable
  */
-export default function(
-    intervalLen: number = 80,
-    startNodeGetter: () => string,
-) {
+export default function(dist: number = 80, getStart: () => string) {
+    // private fields
     let strength: number = 0.1,
         nodes: ShowerNode<any>[],
         hasLinks = false,
         nodeDepth: number[],
         nodesIndexes: Map<string, number>;
 
+    // the main force function
+    // Does nothing if there is no startnode, or no links
     const force = (alpha: number) => {
-        if (!hasLinks || !startNodeGetter()) {
-            return;
-        }
-        for (var i = 0, n = nodes.length, node; i < n; ++i) {
-            (node = nodes[i]),
-                (node.vx +=
-                    (nodeDepth[i] * intervalLen - node.x) * strength * alpha);
+        if (hasLinks && getStart()) {
+            for (let i = 0, n = nodes.length; i < n; ++i) {
+                let node: any = nodes[i];
+                node.vx += (nodeDepth[i] * dist - node.x) * strength * alpha;
+            }
         }
     };
 
+    // Initialiser, store the nodes (called by d3)
     force.initialize = function(inputNodes: ShowerNode<any>[]) {
         nodes = inputNodes;
         nodesIndexes = new Map(
@@ -44,14 +43,17 @@ export default function(
         return arguments.length ? (strength = newStrength) : strength;
     };
 
+    /**
+     * DFS to set depths
+     */
     function setDepths(cur: number, linkMap: number[][], depth: number) {
         if (!(nodeDepth[cur] && nodeDepth[cur] <= depth)) {
             // not visited with lower depth
             nodeDepth[cur] = depth;
-            const n = linkMap[cur].length;
+
             const nextDepth = depth + 1;
-            for (let i = 0; i < n; ++i) {
-                setDepths(linkMap[cur][i], linkMap, nextDepth);
+            for (let child of linkMap[cur]) {
+                setDepths(child, linkMap, nextDepth);
             }
         }
     }
@@ -59,26 +61,23 @@ export default function(
     force.links = (
         links: { source: { id: string }; target: { id: string } }[],
     ) => {
-        if (startNodeGetter()) {
-            const n = nodes.length;
-            const linkMap = new Array(nodes.length);
-            for (let i = 0; i < n; ++i) {
-                linkMap[i] = [];
-            }
+        const startNode = getStart() ?? null;
+        const n = nodes.length;
+        if (startNode !== null && n > 0) {
+            const linkMap: number[][] = Array.from({ length: n }, () => []);
+
             for (const { source, target } of links) {
-                const s = nodesIndexes.get(source.id);
-                const t = nodesIndexes.get(target.id);
-                if (s !== undefined && t !== undefined) {
+                const s = nodesIndexes.get(source.id) ?? null;
+                const t = nodesIndexes.get(target.id) ?? null;
+                if (s !== null && t !== null) {
                     linkMap[s].push(t);
                 }
             }
 
-            nodeDepth = new Array(n);
-            for (let i = 0; i < n; ++i) {
-                nodeDepth[i] = null;
-            }
+            // prepare node depts map
+            nodeDepth = new Array(n).fill(null);
 
-            setDepths(nodesIndexes.get(startNodeGetter()), linkMap, 0);
+            setDepths(nodesIndexes.get(getStart()), linkMap, 0);
 
             hasLinks = true;
         }
