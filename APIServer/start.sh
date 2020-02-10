1b73dd1824aaa7ee1e1081248b83b738892a55ed
beardhatcode <Robbert.GurdeepSingh@UGent.be>
2020-02-10 18:26:13 +0100

    style

--- APIServer/start.sh
+++ APIServer/start.sh
@@ -3,14 +3,25 @@
 # By Robbert Gurdeep Singh
 ################################################################################
 
-
-command -v node >/dev/null 2>&1 || { echo >&2 "I require node but it's not installed.  Aborting."; exit 1; }
-command -v yarn >/dev/null 2>&1 || \
-    { echo >&2 "I require yarn but it's not installed.  Aborting."; exit 1; }
-command -v realpath >/dev/null 2>&1 || \
-    { echo >&2 "I require realpath but it's not installed. It's most likely in the coreutils package. Aborting."; exit 1; }
-curl -s -I "localhost:8529" | grep 'Server: *ArangoDB' 2>/dev/null >/dev/null ||\
-    { echo >&2 "I require arangoDB to be running on port 8529 but it seems not to be up.  Aborting."; exit 1; }
+command -v node >/dev/null 2>&1 || {
+    echo >&2 "I require node but it's not installed.  Aborting."
+    exit 1
+}
+command -v yarn >/dev/null 2>&1 ||
+    {
+        echo >&2 "I require yarn but it's not installed.  Aborting."
+        exit 1
+    }
+command -v realpath >/dev/null 2>&1 ||
+    {
+        echo >&2 "I require realpath but it's not installed. It's most likely in the coreutils package. Aborting."
+        exit 1
+    }
+curl -s -I "localhost:8529" | grep 'Server: *ArangoDB' 2>/dev/null >/dev/null ||
+    {
+        echo >&2 "I require arangoDB to be running on port 8529 but it seems not to be up.  Aborting."
+        exit 1
+    }
 
 run=1
 dirSet=0
@@ -18,9 +29,9 @@ CURPOS=$(dirname $0)
 CURLOC=$(realpath $CURPOS)
 cd $CURLOC
 
-showHelp () {
-echo "$0 [options]"
-cat <<HELP
+showHelp() {
+    echo "$0 [options]"
+    cat <<HELP
   -b         to build
   -c         to clean db (implies -b)
              if -n
@@ -35,54 +46,66 @@ needsBuild=0
 needsClean=0
 dirMakeIfNotExist=0
 
-
 while getopts "h?bcnmd:" opt; do
     case "$opt" in
-        h|\?)
-            showHelp
-            exit 0
-            ;;
-        b)  needsBuild=1
-            ;;
-        c)  needsBuild=1
-            needsClean=1
-            ;;
-        n)  run=0
-            ;;
-        d)  dirSet=1
-            DATADIR=$OPTARG
-            ;;
-        m)  dirMakeIfNotExists=1
-            ;;
-        *)
-            showHelp
-            exit 1
+    h | \?)
+        showHelp
+        exit 0
+        ;;
+    b)
+        needsBuild=1
+        ;;
+    c)
+        needsBuild=1
+        needsClean=1
+        ;;
+    n)
+        run=0
+        ;;
+    d)
+        dirSet=1
+        DATADIR=$OPTARG
+        ;;
+    m)
+        dirMakeIfNotExists=1
+        ;;
+    *)
+        showHelp
+        exit 1
+        ;;
     esac
 done
 
-
 if [ $needsBuild -eq 1 ]; then
     echo "Strating build"
-    yarn install || \
-        ( echo "could install depenencies, is yarn instlled?" ; exit 1)
-    yarn run grunt || \
-        ( echo "could not build the server" ; exit 1)
+    yarn install ||
+        (
+            echo "could install depenencies, is yarn instlled?"
+            exit 1
+        )
+    yarn run grunt ||
+        (
+            echo "could not build the server"
+            exit 1
+        )
 fi
 
 if [ $needsClean -eq 1 ]; then
     echo "Starting clean"
-    node setup.js || \
-        ( echo "Could not clean database" ; exit 1)
+    node setup.js ||
+        (
+            echo "Could not clean database"
+            exit 1
+        )
 fi
 
 if [ $dirSet -eq 1 ]; then
     echo "Checking given directory"
     DATADIR="${DATADIR%/}"
-    if [ ! -d "$DATADIR" ]
-    then
+    if [ ! -d "$DATADIR" ]; then
         if [ $dirMakeIfNotExists -eq 1 ]; then
             echo "Creating $DATADIR"
-            mkdir -p "$DATADIR" || echo "Dir could not be created";
+            mkdir -p "$DATADIR" || echo "Dir could not be created"
         fi
         if [ ! -d "$DATADIR" ]; then
             echo "Stopping"
@@ -94,15 +117,12 @@ if [ $dirSet -eq 1 ]; then
         rm -r "$DATADIR"/*
     fi
 else
-        echo "Making tmp dir"
-        DATADIR=$(mktemp -d)
-        trap "echo STOP;rm -rf '$DATADIR'" EXIT
+    echo "Making tmp dir"
+    DATADIR=$(mktemp -d)
+    trap "echo STOP;rm -rf '$DATADIR'" EXIT
 fi
 
-
-
-if [ $run -eq 1 ]
-then
+if [ $run -eq 1 ]; then
     echo "Starting server"
     node index.js "$DATADIR"
 else
